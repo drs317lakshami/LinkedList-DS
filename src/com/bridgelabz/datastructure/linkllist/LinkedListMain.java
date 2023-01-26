95b8c7534f52ed41d276cb7d153cfb4b61ef28b7
drs317lakshami <devrajkumar317@gmail.com>
2023-01-26 22:30:09 +0530

    Added : Let's create a simple linkedlist list of 56, 30 and 70

---
+++ src/com/bridgelabz/datastructure/linkllist/LinkedListMain.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.datastructure.linkllist;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the LinkedList program");
+        LinkedList<Integer> list1 = new LinkedList();
+        System.out.println("Create Linked List");
+        list1.push(56);
+        list1.push(30);
+        list1.push(70);
+        list1.print();
+    }
+}
