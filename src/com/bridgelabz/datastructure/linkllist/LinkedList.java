d8433bfdd5c1d9c0b2826e9e1d91ba1460675783
drs317lakshami <devrajkumar317@gmail.com>
2023-01-26 22:40:33 +0530

    Added : Create linked list by adding 30 and 56 to 70

--- src/com/bridgelabz/datastructure/linkllist/LinkedList.java
+++ src/com/bridgelabz/datastructure/linkllist/LinkedList.java
@@ -1,31 +1,38 @@
 package com.bridgelabz.datastructure.linkllist;
 
-public class LinkedList<T> {
-	Node<T> head;
-	Node<T> tail;
+public class LinkedList <T>{
+    Node<T> head;
+    Node<T> tail;
+    public void add(T data) {
+        Node<T> newMyNode = new Node(data);
+        if (head == null) {
+            head = newMyNode;
+            tail = newMyNode;
+        } else {
+            tail.setNext(newMyNode);
+            tail = newMyNode;
+        }
+    }public void push(T data) {
+        Node<T> newMyNode = new Node(data);
+        if (head == null) {
+            head = newMyNode;
+            tail = newMyNode;
+        } else {
+            newMyNode.setNext(head);
+            head = newMyNode;
+        }
+    }
 
-	public void push(T data) {
-		Node<T> newMyNode = new Node(data);
-		if (head == null) {
-			head = newMyNode;
-			tail = newMyNode;
-		} else {
-			newMyNode.setNext(head);
-			head = newMyNode;
-		}
-	}
-
-	public void print() {
-		if (head == null) {
-			System.out.println("Linked List is Empty");
-		} else {
-			Node<T> currNode = head;
-			while (currNode != null) {
-				System.out.print(currNode.getData() + " ");
-				currNode = currNode.getNext();
-			}
-			System.out.println();
-		}
-	}
-
+    public void print(){
+        if (head == null){
+            System.out.println("Linked List is Empty");
+        }else {
+            Node<T> currNode = head;
+            while (currNode != null){
+                System.out.print(currNode.getData()+" ");
+                currNode = currNode.getNext();
+            }
+            System.out.println();
+        }
+    }
 }
