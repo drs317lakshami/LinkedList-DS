ade215562967fadc740684b8fb72a9bbb9c3e643
drs317lakshami <devrajkumar317@gmail.com>
2023-01-26 23:14:14 +0530

    Added : Ability to delete the first element in the linkedlist

--- src/com/bridgelabz/datastructure/linkllist/Node.java
+++ src/com/bridgelabz/datastructure/linkllist/Node.java
@@ -3,9 +3,11 @@ package com.bridgelabz.datastructure.linkllist;
 public class Node <T>{
     private T data;
     private Node<T> next;
+
     public Node(T data) {
         this.data = data;
     }
+
     public T getData() {
         return data;
     }
